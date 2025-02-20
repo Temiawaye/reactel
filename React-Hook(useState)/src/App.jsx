3a22866ad713ad022c639680e689b17c77d84473
Temiawaye <awayeteamiloluwa@gmail.com>
2025-02-20 16:02:18 +0100

    i be dev like this?

--- React-Hook(useState)/src/App.jsx
+++ React-Hook(useState)/src/App.jsx
@@ -1,13 +1,15 @@
 import MyComponent from './MyComponent.jsx';
 import Counter from './Counter.jsx';
+import OnChange from './OnChange.jsx';
+import ColorPicker from './ColorPicker.jsx';
 
 function App() {
 
 
   return (
     <>
-      <MyComponent />
-      <Counter />
+      <OnChange />
+      <ColorPicker />
     </>
   )
 }
